76894603a464b9ce085daf1220e1f4f6529f1a8a
Mhugi-HXYZ <mhugisig@gmail.com>
2023-01-25 09:45:07 +0000

    First commit

---
+++ src/main/java/xyz/handshot/shc/RespawnTask.java
@@ -0,0 +1,25 @@
+package xyz.handshot.shc;
+
+import net.minecraft.server.dedicated.MinecraftDedicatedServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.GameMode;
+
+public class RespawnTask implements Runnable {
+    private final MinecraftDedicatedServer server;
+
+    public RespawnTask(MinecraftDedicatedServer server) {
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+        server.getPlayerManager().getPlayerList().forEach(this::respawnIfDead);
+    }
+
+    private void respawnIfDead(ServerPlayerEntity player) {
+        if (!player.isSpectator()) return;
+        player.changeGameMode(GameMode.SURVIVAL);
+        player.requestRespawn();
+        player.setHealth(player.getMaxHealth());
+    }
+}
---
+++ src/main/java/xyz/handshot/shc/SemiHardcore.java
@@ -0,0 +1,34 @@
+package xyz.handshot.shc;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.server.dedicated.MinecraftDedicatedServer;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
+public class SemiHardcore implements DedicatedServerModInitializer {
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    private MinecraftDedicatedServer server;
+
+    @Override
+    public void onInitializeServer() {
+        ServerLifecycleEvents.SERVER_STARTED.register(srv -> {
+            server = (MinecraftDedicatedServer) srv;
+        });
+        ServerPlayConnectionEvents.JOIN.register(new JoinListener(server));
+        executor.scheduleAtFixedRate(new RespawnTask(server), calculateDelay(), TimeUnit.MILLISECONDS.convert(7, TimeUnit.DAYS), TimeUnit.MILLISECONDS);
+    }
+
+    private long calculateDelay() {
+        LocalDateTime nextMonday = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+        return LocalDateTime.now().until(nextMonday, ChronoUnit.MILLIS);
+    }
+}
